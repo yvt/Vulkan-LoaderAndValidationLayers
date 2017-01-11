4a90bcd59fe7490863b2db24c8bc32363000d63b
Mark Lobodzinski <mark@lunarg.com>
2017-01-11 09:16:52 -0700

    layers: Create image validation module
    As image layer is folded into CV, the image-related checks will
    reside in these modules.
    
    Change-Id: I370535c1a2553a0a83b688b87932b6bb1ae8f798

---
+++ layers/image_validation.h
@@ -0,0 +1,49 @@
+/* Copyright (c) 2015-2016 The Khronos Group Inc.
+ * Copyright (c) 2015-2016 Valve Corporation
+ * Copyright (c) 2015-2016 LunarG, Inc.
+ * Copyright (C) 2015-2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Mark Lobodzinski <mark@lunarg.com>
+ */
+#ifndef CORE_VALIDATION_IMAGE_VALIDATION_H_
+#define CORE_VALIDATION_IMAGE_VALIDATION_H_
+
+#include "core_validation_error_enums.h"
+#include "vk_validation_error_messages.h"
+#include "core_validation_types.h"
+#include "vk_layer_logging.h"
+#include "vk_layer_utils.h"
+#include "vk_safe_struct.h"
+#include "vulkan/vk_layer.h"
+#include <map>
+#include <memory>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // CORE_VALIDATION_IMAGE_VALIDATION_H_
