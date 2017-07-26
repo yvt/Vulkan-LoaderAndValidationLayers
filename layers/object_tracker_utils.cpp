03a6375de7ea93278a91fa5d89344db904aaee7b
Mark Lobodzinski <mark@lunarg.com>
2017-07-26 11:19:01 -0600

    layers: Manually validate CmdPushDescSets in OT
    Change-Id: Ife9ebedc5649e12f4f32bd42defa3a9950695977

--- layers/object_tracker_utils.cpp
+++ layers/object_tracker_utils.cpp
@@ -190,6 +190,68 @@ bool ValidateDescriptorSet(VkDevice device, VkDescriptorPool descriptor_pool, Vk
     return skip;
 }
 
+VKAPI_ATTR void VKAPI_CALL CmdPushDescriptorSetKHR(VkCommandBuffer commandBuffer, VkPipelineBindPoint pipelineBindPoint,
+                                                   VkPipelineLayout layout, uint32_t set, uint32_t descriptorWriteCount,
+                                                   const VkWriteDescriptorSet *pDescriptorWrites) {
+    bool skip = false;
+    {
+        std::lock_guard<std::mutex> lock(global_lock);
+        skip |= ValidateObject(commandBuffer, commandBuffer, kVulkanObjectTypeCommandBuffer, false, VALIDATION_ERROR_1be02401,
+                               VALIDATION_ERROR_1be00009);
+        skip |= ValidateObject(commandBuffer, layout, kVulkanObjectTypePipelineLayout, false, VALIDATION_ERROR_1be0be01,
+                               VALIDATION_ERROR_1be00009);
+        if (pDescriptorWrites) {
+            for (uint32_t index0 = 0; index0 < descriptorWriteCount; ++index0) {
+                if ((pDescriptorWrites[index0].descriptorType == VK_DESCRIPTOR_TYPE_UNIFORM_TEXEL_BUFFER) ||
+                    (pDescriptorWrites[index0].descriptorType == VK_DESCRIPTOR_TYPE_STORAGE_TEXEL_BUFFER)) {
+                    for (uint32_t index1 = 0; index1 < pDescriptorWrites[index0].descriptorCount; ++index1) {
+                        if (pDescriptorWrites[index0].pTexelBufferView[index1]) {
+                            skip |= ValidateObject(commandBuffer, pDescriptorWrites[index0].pTexelBufferView[index1],
+                                                   kVulkanObjectTypeBufferView, false, VALIDATION_ERROR_UNDEFINED,
+                                                   VALIDATION_ERROR_15c00009);
+                        }
+                    }
+                }
+                if (pDescriptorWrites[index0].descriptorType == VK_DESCRIPTOR_TYPE_SAMPLER) {
+                    for (uint32_t index1 = 0; index1 < pDescriptorWrites[index0].descriptorCount; ++index1) {
+                        if (pDescriptorWrites[index0].pImageInfo[index1].imageView) {
+                            skip |= ValidateObject(commandBuffer, pDescriptorWrites[index0].pImageInfo[index1].sampler,
+                                                   kVulkanObjectTypeSampler, false, VALIDATION_ERROR_UNDEFINED,
+                                                   VALIDATION_ERROR_04600009);
+                        }
+                    }
+                }
+                if ((pDescriptorWrites[index0].descriptorType == VK_DESCRIPTOR_TYPE_SAMPLED_IMAGE) ||
+                    (pDescriptorWrites[index0].descriptorType == VK_DESCRIPTOR_TYPE_STORAGE_IMAGE) ||
+                    (pDescriptorWrites[index0].descriptorType == VK_DESCRIPTOR_TYPE_INPUT_ATTACHMENT)) {
+                    for (uint32_t index1 = 0; index1 < pDescriptorWrites[index0].descriptorCount; ++index1) {
+                        if (pDescriptorWrites[index0].pImageInfo[index1].imageView) {
+                            skip |= ValidateObject(commandBuffer, pDescriptorWrites[index0].pImageInfo[index1].imageView,
+                                                   kVulkanObjectTypeImageView, false, VALIDATION_ERROR_UNDEFINED,
+                                                   VALIDATION_ERROR_04600009);
+                        }
+                    }
+                }
+                if ((pDescriptorWrites[index0].descriptorType == VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER) ||
+                    (pDescriptorWrites[index0].descriptorType == VK_DESCRIPTOR_TYPE_STORAGE_BUFFER) ||
+                    (pDescriptorWrites[index0].descriptorType == VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC) ||
+                    (pDescriptorWrites[index0].descriptorType == VK_DESCRIPTOR_TYPE_STORAGE_BUFFER_DYNAMIC)) {
+                    for (uint32_t index1 = 0; index1 < pDescriptorWrites[index0].descriptorCount; ++index1) {
+                        if (pDescriptorWrites[index0].pBufferInfo[index1].buffer) {
+                            skip |= ValidateObject(commandBuffer, pDescriptorWrites[index0].pBufferInfo[index1].buffer,
+                                                   kVulkanObjectTypeBuffer, false, VALIDATION_ERROR_04401a01,
+                                                   VALIDATION_ERROR_UNDEFINED);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (skip) return;
+    get_dispatch_table(ot_device_table_map, commandBuffer)
+        ->CmdPushDescriptorSetKHR(commandBuffer, pipelineBindPoint, layout, set, descriptorWriteCount, pDescriptorWrites);
+}
+
 void CreateQueue(VkDevice device, VkQueue vkObj) {
     layer_data *device_data = GetLayerDataPtr(get_dispatch_key(device), layer_data_map);
 
